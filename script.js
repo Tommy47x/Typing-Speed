fb9091c3d9f69fdc1b1d3be164eacb81c2e6793b
Tommy47x <cristianleulbdt@gmail.com>
2023-10-09 19:50:51 +0300

    -modified the order of the functions -removed useless comments This change has been made throught VSC Terminal

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ inputWord.disabled = true; // Disable input until game starts
 let spacebarListener = null; // Declare the event listener variable outside the function
 //Also this variable is used to remove the event listener when the game ends, so it doesn't keep listening for the spacebar
 
-//write a function that will start the game when the user clicks the start button 
-
 // Variables for the game
 let currentIndex = 0;
 let correctCount = 0;
@@ -15,25 +13,22 @@ let typedWord = "";
 
 // Function that starts the game when the user clicks the start button
 function startGame() {
+    addKeyDownEventListener(); // Add event listener for the spacebar
     displayTime(); // Display the timer
     inputWord.disabled = false; // Enable input
     textArray = generateRandomTextArray(); // Generate the random array
     displayText(); //Display the word
-    addKeyDownEventListener(); // Add event listener for the spacebar
 }
 
-function enableNightMode() { // Function that enables night mode (dark theme)
-    const body = document.body;
-    body.style.backgroundColor = "#222";
-    body.style.color = "gray";
-    //All elements except the margins of the alert container will have the same background color and text color
-    const elements = document.querySelectorAll('body *:not(.alert-container)');
-    for (let i = 0; i < elements.length; ++i) {
-        const inPageElements = elements[i];
-        inPageElements.style.backgroundColor = "#222";
-        inPageElements.style.color = "gray";
-    }
+// Function that adds the event listener for the keydown event (SPACE)
+function addKeyDownEventListener() {
+    spacebarListener = document.addEventListener("keydown", function (event) {
+        if (event.code === "Space") {
+            checkWord();
+        }
+    });
 }
+
 
 function displayTime() { // Function that displays the timer
     let time = 59;
@@ -46,29 +41,19 @@ function displayTime() { // Function that displays the timer
     }, 1000);
 }
 
-// Function that adds the event listener for the keydown event (SPACE)
-function addKeyDownEventListener() {
-    spacebarListener = document.addEventListener("keydown", function (event) {
-        if (event.code === "Space") {
-            checkWord();
-        }
-    });
-}
-
 // Function that generates the random array
 function generateRandomTextArray() {
     const words = ["algorithm", "array", "boolean", "class", "compiler", "constant", "database", "debugging", "function", "inheritance", "interface", "iteration", "javascript", "library", "loop", "method", "object", "operator", "parameter", "recursion", "variable", "webpage", "abstraction", "agile", "api", "architecture", "assembly", "backend", "binary", "bug", "bytecode", "cache", "char", "client", "cloud", "code", "command", "compile", "console", "cookie", "css", "data", "debug", "declare", "deploy", "design", "devops", "directive", "dns", "dom", "dynamic", "element", "encryption", "error", "event", "exception", "expression", "file", "firewall", "framework", "frontend", "git", "gui", "hash", "html", "http", "https", "ide", "immutable", "inherit", "instance", "int", "interface", "interpreter", "java", "json", "kernel", "library", "linker", "linux", "load", "local", "logic", "loop", "machine", "macro", "memory", "metadata", "middleware", "module", "multithreading", "network", "null", "object-oriented", "opcode", "optimization", "os", "package", "parameter", "parse", "performance", "php", "port", "process", "program", "protocol", "query", "queue", "ram", "react", "recursion", "regex", "repository", "request", "response", "rest", "router", "runtime", "sass", "script", "sdk", "security", "server", "session", "socket", "source", "sql", "stack", "static", "storage", "string", "syntax", "tag", "template", "testing", "thread", "token", "tree", "type", "unicode", "unix", "url", "variable", "version", "virtual", "web", "websocket", "windows", "xml", "yaml"];
     const textArray = [];
     for (let i = 0; i < words.length; ++i) {
         const randomWordIndex = Math.floor(Math.random() * words.length);
-        const word = words[randomWordIndex] // Convert word to lowercase
+        const word = words[randomWordIndex] // Get random word from array
         words.splice(randomWordIndex, 1); // Remove word from array so it doesn't repeat
         textArray.push(word); // Add word to array of words to be displayed in the game
     }
     return textArray;
 }
 
-let gameEnded = false;
 const textContainer = document.getElementById("textContainer");
 // Function that displays the actual word
 function displayText() {
@@ -78,6 +63,7 @@ function displayText() {
     }
 }
 
+let gameEnded = false;
 // Function that checks the relation Input - Actual Word
 function checkWord() {
     if (gameEnded) return;
@@ -108,7 +94,7 @@ function checkMatch(typedWord, currentWord) { // Function that checks if the typ
 function endGame() {
     clearInterval(timer); // Stops the timer
     textContainer.innerHTML = `Game Over!
-   <br>
+    <br>
     <button class="btn btn-primary" id="restartButton"onClick ="location.reload()">Restart</button>`
     // Display the results
     document.getElementById("successMessage").textContent = `Correct: ${correctCount}`;
@@ -117,4 +103,17 @@ function endGame() {
     inputWord.disabled = true; // Disable input
     document.removeEventListener("keydown", spacebarListener); // Remove event listener for spacebar
     gameEnded = true;
+}
+
+function enableNightMode() { // Function that enables night mode (dark theme)
+    const body = document.body;
+    body.style.backgroundColor = "#222";
+    body.style.color = "gray";
+    //All elements except the margins of the alert container will have the same background color and text color
+    const elements = document.querySelectorAll('body *:not(.alert-container)');
+    for (let i = 0; i < elements.length; ++i) {
+        const inPageElements = elements[i];
+        inPageElements.style.backgroundColor = "#222";
+        inPageElements.style.color = "gray";
+    }
 }
